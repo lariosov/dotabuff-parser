25d58674cc0edee847d7b2ef8e86421da26f847a
Anton Levitskiy <larios18000@gmail.com>
2024-12-17 23:27:19 +0300

    Fixed stats. Duration was not added in to stats before.

--- main.py
+++ main.py
@@ -6,59 +6,68 @@ st_accept = "text/html"
 st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
 
 # формируем хеш заголовков
-headers = {
-   "Accept": st_accept,
-   "User-Agent": st_useragent
-}
+headers = {"Accept": st_accept, "User-Agent": st_useragent}
+
 
 def start_scrapping(url) -> None:
     req = requests.get(url, headers=headers)
 
     # считываем текст HTML-документа
-    soup = BeautifulSoup(req.text, 'lxml') # достали последние игры
-    games = soup.find('div', 
-                      class_='performances-overview')
-    
+    soup = BeautifulSoup(req.text, "lxml")  # достали последние игры
+    games = soup.find("div", class_="performances-overview")
+
     clear_stats = {}
 
     for stats in games:
-        hero = stats.find('div', class_='r-fluid').find('a').find('img').get('alt')
-        kda = stats.find('span', class_='kda-record').text
-        lvl = stats.find('div', class_='tw-rounded-tl-sm').text
-        match_result = stats.find('div', class_='r-match-result').find('a', class_=['won', 'lost']).text
-        game_mode = stats.find('div', 
-                          class_='r-fluid r-175 r-text-only r-first').find('div', 
-                                                                           class_='r-body').find('div',
-                                                                                                 class_='subtext').text
-        game_link = stats.find('div', 
-                          class_='r-match-result').find('a',
-                                                        class_=['won', 'lost']).get('href')
-        date = stats.find('div',
-                          class_='r-match-result').find('time').text
-        avg_rate = stats.find('div',
-                              class_='subtext').text
-        role = stats.find('span',
-                          class_='subtext icons').find('i',
-                                                        'role-icon').get('title')
-        lane = stats.find('span',
-                          class_='subtext icons').find('i',
-                                                        'lane-icon').get('title')
-    
+        hero = stats.find("div", class_="r-fluid").find("a").find("img").get("alt")
+        kda = stats.find("span", class_="kda-record").text
+        lvl = stats.find("div", class_="tw-rounded-tl-sm").text
+        match_result = (
+            stats.find("div", class_="r-match-result")
+            .find("a", class_=["won", "lost"])
+            .text
+        )
+        game_mode = (
+            stats.find("div", class_="r-first")
+            .find("div", class_="r-body")
+            .find("div", class_="subtext")
+            .text
+        )
+        game_link = (
+            stats.find("div", class_="r-match-result")
+            .find("a", class_=["won", "lost"])
+            .get("href")
+        )
+        date = stats.find("div", class_="r-match-result").find("time").text
+        avg_rate = stats.find("div", class_="subtext").text
+        role = (
+            stats.find("span", class_="subtext icons")
+            .find("i", "role-icon")
+            .get("title")
+        )
+        lane = (
+            stats.find("span", class_="subtext icons")
+            .find("i", "lane-icon")
+            .get("title")
+        )
+        duration = stats.find("div", class_="r-duration").find("div", "r-body").text
+
         clear_stats[game_link] = {
-            'hero': hero,
-            'kda': kda,
-            'game_mode': game_mode,
-            'lvl': lvl,
-            'match_result': match_result,
-            'date': date,
-            'avg_rate': avg_rate,
-            'role': role,
-            'lane': lane
+            "hero": hero,
+            "kda": kda,
+            "game_mode": game_mode,
+            "lvl": lvl,
+            "match_result": match_result,
+            "date": date,
+            "avg_rate": avg_rate,
+            "role": role,
+            "lane": lane,
+            "duration": duration,
         }
-        
+
     return clear_stats
 
 
-if __name__ == '__main__': # бест-практис... ю ноу?
-    url = 'https://ru.dotabuff.com/players/249237243'
+if __name__ == "__main__":  # бест-практис... ю ноу?
+    url = "https://ru.dotabuff.com/players/249237243"
     print(start_scrapping(url=url))
