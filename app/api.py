de9fed4d73eabfa8c364f9f68fe6fd85d422e647
Anton Levitskiy <larios18000@gmail.com>
2024-12-22 21:20:13 +0300

    Logging added into api.py

--- app/api.py
+++ app/api.py
@@ -1,17 +1,41 @@
+# основные импорты
 import main
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, HTTPException
 from typing import Optional
 
 
 app = FastAPI()
 
+# уровень логирования, пока что вносится вручную
+logging.basicConfig(
+    level="INFO",
+    filename="api.log",
+    format="%(asctime)s %(levelname)s %(message)s",
+)
+
+
+@app.get("/")  # путь апи
+def start_screen():
+    logging.info("Used start screen.")
+    raise HTTPException(
+        status_code=400, 
+        detail="You need to use API with '/api/profile'")
+
 
 @app.get("/api/profile")  # путь апи
 def profile_games(id: Optional[int] = None):
     if id is None:
-        return "Variable 'id' is empty."
+        logging.info('Empty "id" variable.')
+        raise HTTPException(status_code=400, 
+                            detail="Your 'id' variable is empty.")
     else:
         try:
+            logging.info("User send request. "
+                         f"DotaBuff id = {id}")
             return main.start_scrapping(id=id)
         except:
-            return "500, Check your 'id' variable."
+            logging.warning("User send bad request. "
+                            f"DotaBuff id = {id}")
+            raise HTTPException(status_code=400, 
+                                detail="Check your 'id' variable.")
