5250b5770c031950a2bc14bdc0ed862866e404cd
Anton Levitsky <larios18000@gmail.com>
2025-03-13 20:27:45 +0400

    dev: api logs have now IP's addresses

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 # основные импорты
 import app.main as main
 import logging
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from typing import Optional
 
 
@@ -16,26 +16,28 @@ logging.basicConfig(
 
 
 @app.get("/")  # путь апи
-def start_screen():
-    logging.info("Used start screen.")
+def start_screen(request: Request):
+    host_ip = request.client.host
+    logging.info(f"{host_ip} : Used start screen.")
     raise HTTPException(
         status_code=400, 
         detail="You need to use API with '/api/profile'")
 
 
 @app.get("/api/profile")  # путь апи
-def profile_games(id: Optional[int] = None):
+def profile_games(request: Request, id: Optional[int] = None):
+    host_ip = request.client.host
     if id is None:
-        logging.info('Empty "id" variable.')
+        logging.info(f'{host_ip} : Empty "id" variable.')
         raise HTTPException(status_code=400, 
                             detail="Your 'id' variable is empty.")
     else:
         try:
-            logging.info("User send request. "
+            logging.info(f"{host_ip} : request. "
                          f"DotaBuff id = {id}")
             return main.start_scrapping(id=id)
         except:
-            logging.warning("User send bad request. "
+            logging.warning(f"{host_ip} bad request. "
                             f"DotaBuff id = {id}")
             raise HTTPException(status_code=400, 
                                 detail="Check your 'id' variable.")
