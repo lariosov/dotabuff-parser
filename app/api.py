de1b56012e52ad091e33a4533e77cc4d9d79289b
Anton Levitsky <larios18000@gmail.com>
2025-06-19 18:27:30 +0400

    dev: added prometheus metrics for grafana visualisation

--- app/api.py
+++ app/api.py
@@ -1,10 +1,12 @@
 # основные импорты
 import app.main as main
 import logging
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Response
 from typing import Optional
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
 app = FastAPI()
 
 # уровень логирования, пока что вносится вручную
@@ -17,6 +19,7 @@ logging.basicConfig(
 
 @app.get("/")  # путь апи
 def start_screen(request: Request):
+    REQUEST_COUNT.inc()
     host_ip = request.client.host
     logging.info(f"{host_ip} : Used start screen.")
     raise HTTPException(
@@ -24,8 +27,20 @@ def start_screen(request: Request):
         detail="You need to use API with '/api/profile'")
 
 
+@app.get("/metrics")
+def metrics(request: Request):
+    client_ip = request.client.host
+    if client_ip not in ["127.0.0.1", "::1"]:
+        raise HTTPException(status_code=403, detail="Access forbidden")
+    return Response(
+        content=generate_latest(),
+        media_type=CONTENT_TYPE_LATEST
+        )
+
+
 @app.get("/api/profile")  # путь апи
 def profile_games(request: Request, id: Optional[int] = None):
+    REQUEST_COUNT.inc()
     host_ip = request.client.host
     if id is None:
         logging.info(f'{host_ip} : Empty "id" variable.')
