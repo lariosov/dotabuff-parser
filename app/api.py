a784174134963118201d83eed9e1b8286b9d236c
Anton Levitsky <larios18000@gmail.com>
2025-06-27 17:27:17 +0400

    hotfix: fixed metrics and access

--- app/api.py
+++ app/api.py
@@ -1,13 +1,20 @@
 # основные импорты
 import app.main as main
-import logging
+import logging, time
 from fastapi import FastAPI, HTTPException, Request, Response
 from typing import Optional
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST, make_asgi_app
+from prometheus_client import Counter, generate_latest,\
+    CONTENT_TYPE_LATEST, make_asgi_app, Histogram
 
 
 REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests',
                         ['method', 'endpoint', 'status_code'],)
+REQUEST_DURATION = Histogram(
+    'http_request_duration_seconds',
+    'Duration of HTTP requests in seconds',
+    ['method', 'endpoint', 'status_code'],
+    buckets=[0.01, 0.05, 0.1, 0.5, 1, 5]
+)
 app = FastAPI()
 
 metrics_app = make_asgi_app()
@@ -26,18 +33,25 @@ logging.basicConfig(
 async def monitor_requests(request: Request,
                            call_next):
     # Сохраняем информацию о запросе ДО обработки
+    start_time = time.monotonic()
     method = request.method
     endpoint = request.url.path
     
     # Выполняем запрос и получаем ответ
     response = await call_next(request)
+    duration = time.monotonic() - start_time
     
-    # Регистрируем метрику ПОСЛЕ получения ответа
+    # Регистрируем метрики ПОСЛЕ получения ответа
     REQUEST_COUNT.labels(
         method=method,
         endpoint=endpoint,
         status_code=response.status_code
     ).inc()
+    REQUEST_DURATION.labels(
+        method=request.method,
+        endpoint=request.url.path,
+        status_code=response.status_code
+    ).observe(duration)
     
     return response
 
@@ -54,7 +68,7 @@ def start_screen(request: Request):
 @app.get("/metrics")
 def metrics(request: Request):
     client_ip = request.client.host
-    if client_ip not in ["127.0.0.1", "::1", "172.17.0.1"]:
+    if client_ip not in ["127.0.0.1", "::1", "172.17.0.1", "138.124.114.181"]:
         raise HTTPException(status_code=403, detail="Access forbidden")
     return Response(
         content=generate_latest(),
